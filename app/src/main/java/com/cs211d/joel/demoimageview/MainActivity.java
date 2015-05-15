daa75dc6a7bc32bb85d07144c04f7bf59fe69ab5
joel rainey <joelrainey@gmail.com>
2015-05-15 10:34:27 -0700

    Added method stubs for prev and next, and general prep for demo of imageview

--- app/src/main/java/com/cs211d/joel/demoimageview/MainActivity.java
+++ app/src/main/java/com/cs211d/joel/demoimageview/MainActivity.java
@@ -1,19 +1,57 @@
 package com.cs211d.joel.demoimageview;
 
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    private ImageView mIv;
+    ArrayList<Integer> al = new ArrayList<>();
+    int next =0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected  void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mIv =(ImageView)findViewById(R.id.iv_pics);
+
+        Field fld[] = com.cs211d.joel.demoimageview.R.drawable.class.getFields();
+
+        for(Field f:fld)
+        {
+            Integer imgId = null;
+            try {
+                imgId = f.getInt(null);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            al.add(imgId);
+        }
+        
     }
 
+
+
+    public void next(View v)
+    {
+        Message.message(getApplicationContext(),"Show Next Pic");
+        mIv.setImageResource(al.get(next++));
+    }
+
+
+    public void prev(View v)
+    {
+        Message.message(getApplicationContext(),"Show Prev Pic");
+
+    }
 }
---
+++ app/src/main/java/com/cs211d/joel/demoimageview/Message.java
@@ -0,0 +1,13 @@
+package com.cs211d.joel.demoimageview;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message
+{
+    public static void message(Context context,String msg)
+    {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+
+    }
+}
